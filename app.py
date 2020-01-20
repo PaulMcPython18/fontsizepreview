d2207d6ed626eca3a32cd5395a1eb783cd37fca8
PaulMcPython18 <57735592+PaulMcPython18@users.noreply.github.com>
2020-01-19 18:05:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,19 @@ def index():
     return render_template('index.html')
 @app.route('/', methods=['POST'])
 def output():
-    namequery1 = request.form['namequery1']
-    namequery2 = request.form['namequery2']
-    fontsize = str(namequery1) + "px"
-    print(fontsize)
-    color = str(namequery2)
-    if len(color) > 100:
-        return render_template('index.html',
-                               other_message="* Sorry! You must input a sequence of characters less than 100! *")
-    print(color)
-    return render_template('index.html', output = 'Random text!', fontstyle=fontsize, fontcolor=color, message="* Not seeing the correct color? Make sure you have inputted an existant color. *")
+    try:
+        namequery1 = request.form['namequery1']
+        namequery2 = request.form['namequery2']
+        fontsize = str(namequery1) + "px"
+        print(fontsize)
+        color = str(namequery2)
+        if len(color) > 100:
+            return render_template('index.html',
+                                   other_message="* Sorry! You must input a sequence of characters less than 100! *")
+        print(color)
+        return render_template('index.html', output = 'Random text!', fontstyle=fontsize, fontcolor=color, message="* Not seeing the correct color? Make sure you have inputted an existant color. *")
+    except:
+        return render_template('index.html', message="* Sorry! An error occured, reload the page and try again! *")
 @app.route('/about')
 def about():
     return render_template('about.html')
